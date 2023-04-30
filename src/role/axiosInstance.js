010f93ef590b544251618987bec6edb8649baa14
BaudelMin <min.baudel09@gmail.com>
2023-04-30 13:05:43 +0545

    cors properly solved

--- src/role/axiosInstance.js
+++ src/role/axiosInstance.js
@@ -1,18 +1,24 @@
 import axios from "axios";
 import getCookie from "../tokens/DjangoCSRFToken";
+import cors from 'cors'
 
 const csrfToken = getCookie('csrftoken')
 console.log('csrf Token = ', csrfToken)
 
+const corsMiddleware = cors({
+    origin: 'http://localhost:8000',  // replace with your Django API's domain
+    credentials: true,
+  });
+
 const axiosInstance = axios.create({
-    // baseURL:'http://127.0.0.1:8000',
-    baseURL:'http://localhost:3000/',
+    baseURL:'http://127.0.0.1:8000',
+    // baseURL:'http://localhost:3000/',
     headers:{
-        'Access-Control-Allow-Headers': 'X-Requested-With, privatekey',
-        "Access-Control-Allow-Origin": "*",
         // 'X-CSRFToken':csrfToken
         // 'X-CSRFToken': '{{ csrf_token }}'
-    }
+    },
+    mode:'cors',
+    credentials: 'include',
 })
 
 export default axiosInstance
